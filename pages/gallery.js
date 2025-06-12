9167b69b28169e4eabac98fc775d43339d688303
Plk-3 <perrbear0519@gmail.com>
2025-06-12 09:03:22 -0400

    Create gallery.js

---
+++ pages/gallery.js
@@ -0,0 +1,8 @@
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Gallery</h1>
+      <p>Gallery images will appear here. (Connect images via Netlify CMS!)</p>
+    </section>
+  )
+}
