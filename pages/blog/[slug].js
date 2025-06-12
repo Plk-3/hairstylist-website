78ff4d0f777e5c5bfc17ebe993fbac89ebb2839a
Plk-3 <perrbear0519@gmail.com>
2025-06-12 09:06:14 -0400

    Create [slug].js

---
+++ pages/blog/[slug].js
@@ -0,0 +1,27 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync('content/blog')
+  const paths = files.map(filename => ({
+    params: { slug: filename.replace('.md', '') }
+  }))
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps({ params }) {
+  const markdownWithMeta = fs.readFileSync(path.join('content/blog', params.slug + '.md'), 'utf-8')
+  const { data, content } = matter(markdownWithMeta)
+  return { props: { data, content } }
+}
+
+export default function BlogPost({ data, content }) {
+  return (
+    <article>
+      <h1>{data.title}</h1>
+      <small>{data.date}</small>
+      <div>{content}</div>
+    </article>
+  )
+}
